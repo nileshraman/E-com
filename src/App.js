6e9d9499761778c0fa10de0f3b2e558a604b6dd5
nileshraman <ramanrdj@gmail.com>
2024-08-06 15:21:06 +0530

    removed unwanted code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
-// import Cart from './pages/Cart'; // Ensure the correct path to your Cart component
-// import NavBar from './components/navbar';
 import ShoppingCart from './pages/home';
 
 const App = () => {
